50f0b41c845b8e12bafd5f0f87d0b3090eb75277
t.masuda <masuda@moonmile.net>
2018-07-15 14:53:29 +0900

    NetScrattino を .NET Standard 化

---
+++ src/NetScrattino.Core/Firmata.fs
@@ -0,0 +1,150 @@
+﻿namespace FirmataNET
+
+open System
+open System.IO.Ports
+
+/// Firmata on Arduino class
+/// refer http://firmata.org/wiki/Protocol
+type Arduino() =
+    let DIGITAL_MESSAGE = 0x90uy    // send data for a digital port
+    let ANALOG_MESSAGE = 0xE0uy     // send data for an analog pin (or PWM)
+    let REPORT_ANALOG = 0xC0uy      // enable analog input by pin #
+    let REPORT_DIGITAL = 0xD0uy     // enable digital input by port
+    let SET_PIN_MODE = 0xF4uy       // set a pin to INPUT/OUTPUT/PWM/etc
+    let REPORT_VERSION = 0xF9uy     // report firmware version
+    let SYSTEM_RESET = 0xFFuy       // reset from MIDI
+    let START_SYSEX = 0xF0uy        // start a MIDI SysEx message
+    let END_SYSEX = 0xF7uy          // end a MIDI SysEx message
+
+    let mutable _socket:SerialPort = null
+    let mutable _portName = "COM3"
+    let mutable _baudRate = 57600
+ 
+    // let digitalOutputData = Array.zeroCreate(16)
+    let digitalInputData = Array.zeroCreate(16)
+    let analogInputData = Array.zeroCreate(16)
+ 
+    /// Connect Serial on Arduino.
+    member this.Connect( port:string ) =
+        _socket <- new SerialPort( port, _baudRate )
+        _socket.Open()
+ 
+    member this.Open() =
+        this.Reset()
+        // read data form Arduino
+        _socket.DataReceived.Add( fun (e) -> 
+            while _socket.BytesToRead > 0 do
+                let head = _socket.ReadByte() |> byte
+                match head with
+                // analog 14-bit data format
+                // 0  analog pin, 0xE0-0xEF, (MIDI Pitch Wheel)
+                // 1  analog least significant 7 bits
+                // 2  analog most significant 7 bits
+                | h when ANALOG_MESSAGE <= h && h <= ANALOG_MESSAGE + 15uy -> 
+                    let pin = int(h - ANALOG_MESSAGE)
+                    let lsb = _socket.ReadByte()
+                    let msb = _socket.ReadByte()
+                    let data = (msb <<< 7) ||| lsb
+                    analogInputData.[pin] <- data
+                // two byte digital data format, second nibble of byte 0 gives the port number (e.g. 0x92 is the third port, port 2)
+                // 0  digital data, 0x90-0x9F, (MIDI NoteOn, but different data format)
+                // 1  digital pins 0-6 bitmask
+                // 2  digital pin 7 bitmask 
+                | h when DIGITAL_MESSAGE <= h && h <= DIGITAL_MESSAGE + 15uy -> 
+                    let pin = int(h - DIGITAL_MESSAGE)
+                    let lsb = _socket.ReadByte()
+                    let msb = _socket.ReadByte()
+                    let data = (msb <<< 7) ||| lsb
+                    digitalInputData.[pin] <- data
+                | _ -> 
+                    // read off
+                    let d = _socket.ReadExisting()
+                    ()
+        )
+ 
+    member this.Close() = 
+        _socket.Close()
+        _socket <- null
+ 
+    member this.Reset() =
+        for i=0 to 5 do
+            let command = [|
+                REPORT_ANALOG ||| byte(i)
+                1uy
+            |]
+            _socket.Write( command, 0, command.Length )
+        for i=0 to 1 do
+            let command = [|
+                REPORT_DIGITAL ||| byte(i)
+                1uy
+            |]
+            _socket.Write( command, 0, command.Length )
+
+    member this.digitalRead(pin:int):int =
+        (digitalInputData.[pin >>> 3] >>> (pin &&& 0x07)) &&& 0x01
+    member this.analogRead(pin:int):int =
+        analogInputData.[pin]
+ 
+    // analog 14-bit data format
+    // 0  analog pin, 0xE0-0xEF, (MIDI Pitch Wheel)
+    // 1  analog least significant 7 bits
+    // 2  analog most significant 7 bits
+    member this.pinMode(pin,mode) =
+        let message = [|
+            SET_PIN_MODE
+            byte(pin)
+            byte(mode)
+        |]
+        _socket.Write( message, 0, message.Length )
+ 
+    // two byte digital data format, second nibble of byte 0 gives the port number (e.g. 0x92 is the third port, port 2)
+    // 0  digital data, 0x90-0x9F, (MIDI NoteOn, but different data format)
+    // 1  digital pins 0-6 bitmask
+    // 2  digital pin 7 bitmask 
+    member this.digitalWrite(pin,value) =
+        let portNumber = (pin >>> 3) &&& 0xFF
+        digitalInputData.[portNumber] <-
+            if value = 0 then
+                digitalInputData.[portNumber] &&& ~~~(1 <<< (pin &&& 0x07))
+            else
+                digitalInputData.[portNumber] ||| (1 <<< (pin &&& 0x07)) 
+        let message = [|
+            DIGITAL_MESSAGE ||| byte(portNumber) 
+            byte(digitalInputData.[portNumber] &&& 0x7F)
+            byte(digitalInputData.[portNumber] >>> 7)
+        |]
+        _socket.Write(message, 0, message.Length);
+             
+    // analog 14-bit data format
+    // 0  analog pin, 0xE0-0xEF, (MIDI Pitch Wheel)
+    // 1  analog least significant 7 bits
+    // 2  analog most significant 7 bits
+    member this.analogWrite(pin,value) = 
+        let message = [|
+            ANALOG_MESSAGE ||| (byte(pin) &&& 0x0Fuy)
+            byte(value &&& 0x7F)
+            byte(value >>> 7)
+        |]
+        _socket.Write(message, 0, message.Length);
+ 
+    // toggle analogIn reporting by pin
+    // 0  toggle analogIn reporting (0xC0-0xCF) (MIDI Program Change)
+    // 1  disable(0)/enable(non-zero) 
+    member this.analogReport(pin, enable) =
+        let message = [|
+            REPORT_ANALOG ||| (byte(pin) &&& 0xFuy)
+            byte(if enable then 1 else 0)
+        |]
+        _socket.Write(message, 0, message.Length);
+
+    // toggle digital port reporting by port (second nibble of byte 0), e.g. 0xD1 is port 1 is pins 8 to 15,  
+    // 0  toggle digital port reporting (0xD0-0xDF) (MIDI Aftertouch)
+    // 1  disable(0)/enable(non-zero) 
+    member this.digitalReport(pin, enable) =
+        let message = [|
+            REPORT_DIGITAL ||| (byte(pin) >>> 3) &&& 0xFFuy
+            (1uy <<< (pin &&& 0x07))
+        |]
+        _socket.Write(message, 0, message.Length);
+
+       
---
+++ src/NetScrattino.Core/Program.fs
@@ -0,0 +1,82 @@
+﻿module Program
+
+open System
+
+let mutable arduino = new FirmataNET.Arduino()
+
+// Scratchから受信するためのHTTPサーバー
+let Server( port ) =
+    let listener = new System.Net.HttpListener()
+    listener.Prefixes.Add("http://127.0.0.1:"+(port |> string)+"/" )
+    listener.Start()
+    while true do
+        let context = listener.GetContext()
+        let res = context.Response
+        let mutable data = ""
+        let path = context.Request.Url.PathAndQuery
+        match path with 
+            | "/poll" -> 
+                for i=0 to 5 do 
+                    data <- data + String.Format("a{0} {1}\n", i, arduino.analogRead(i))
+                for i=2 to 13 do 
+                    data <- data + String.Format("d{0} {1}\n", i, arduino.digitalRead(i))
+                // デバッグ出力
+                let mutable debug = ""
+                for i=0 to 5 do 
+                    debug <- debug + String.Format("a{0} {1} ", i, arduino.analogRead(i))
+                debug <- debug + "\n"
+                for i=2 to 13 do 
+                    debug <- debug + String.Format("d{0} {1} ", i, arduino.digitalRead(i))
+                debug <- debug + "\n"
+                // printfn "%s" path
+                // printfn "%s" debug
+            | "/reset_all" ->
+                printfn "/reset_all"
+                arduino.Reset()
+                data <- "ok"
+            | _ ->
+                let pa = path.Split([|'/'|])
+                match pa.[1] with   
+                | "digitalWrite" ->
+                    let pin = pa.[2].Substring(1) |> int
+                    let value = 
+                        match pa.[3].ToUpper() with
+                        | "ON" -> 1
+                        | "OFF" -> 0
+                        | _ -> pa.[3] |> int
+                    arduino.digitalWrite( pin, value )
+                | "analogWrite" ->
+                    let pin = pa.[2].Substring(1) |> int
+                    let value = pa.[3] |> int
+                    arduino.pinMode( pin, 0x03 )    // PWM
+                    arduino.analogWrite( pin, value )
+                | "servoWrite" ->
+                    let pin = pa.[2].Substring(1) |> int
+                    let value = pa.[3] |> int
+                    arduino.pinMode( pin, 0x04 )    // SERVO
+                    arduino.analogWrite( pin, value )
+                | "setMode" ->
+                    let pin = pa.[2].Substring(1) |> int
+                    let value = if pa.[3] = "PULLUP" then 0x0B else 0x00
+                    arduino.pinMode( pin, value )
+                | "setPinMode" ->
+                    let pin = pa.[2].Substring(1) |> int
+                    let value = 
+                        match pa.[3] with
+                        | "INPUT" -> 0
+                        | "OUTPUT" -> 1
+                        | "PWM" -> 3
+                        | "SERVO" -> 4
+                        | "INPUT_PULLUP" -> 11
+                        | _ -> 1
+                    arduino.pinMode( pin, value )
+                | _ ->
+                    data <- ""
+                printfn "%s" path
+        res.StatusCode <- 200
+        let sw = new System.IO.StreamWriter( res.OutputStream )
+        sw.Write( data )
+        sw.Close()
+    ()
+
+
